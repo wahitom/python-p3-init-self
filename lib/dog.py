b6b2e16126b6b6c3548e629b2af921a19600b869
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-11-29 20:44:36 +0300

    Complete"

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    
+    def __init__(self, name, breed ="Mutt"):
+        self.name = name 
+        self.breed = breed
+
+fido = Dog("Fido", "Chihuahua")    
+print(fido.name)  
+print(fido.breed)
+
